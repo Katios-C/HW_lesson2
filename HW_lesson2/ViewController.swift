3e0a2f292b4dd56f65a9a255ba3c77a08b792651
Katios <kate.chonkina@gmail.com>
2020-10-20 14:18:39 +0600

    Initial Commit

---
+++ HW_lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_lesson2
+//
+//  Created by Екатерина Чернова on 20.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
